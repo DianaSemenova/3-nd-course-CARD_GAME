cfef2f93f5156ed3213a1bb4d8907b7d8c4e4cdf
DianaSemenova <menestrelia199702@gmail.com>
2023-08-01 20:34:36 +0400

    рендеритм страница с картами (экраны 1,2)

--- components/render_level_game.js
+++ components/render_level_game.js
@@ -1,9 +1,58 @@
+import { cardsSuitsArr } from "./arrayCardsSuits.js"
+
 export function renderLevelGame(level, appEl) {
-    const appHTML = `
-            <div class="top center">
-            <div class="box-game">       
-            <h2 class="box-game__title">${level.value} карточек</h2>
-            </div></div>
-            `;
+
+let levelGame = level.value;
+const cardsFlipSide = [];
+//let clickCards = false;
+
+function getCardsFlipSideArr(levelGame) {
+
+    for (let i = 0; i < levelGame; i++) {
+        cardsFlipSide.push(`<img id="cards-click" class="game-cards__flip-side" src="./img/рубашка.png">`);
+    }; 
+    return cardsFlipSide;   
+}
+
+getCardsFlipSideArr(levelGame);
+
+    const appHTML = `   
+    <div class="game-cards">
+     <header class="game-cards-timer">
+         <div class="game-cards-timer__module">  
+            <div class="game-cards-timer__init">
+                <p class="game-cards-timer__item">min</p>
+                <p class="game-cards-timer__item">sec</p>
+            </div>   
+             <p class="game-cards-timer__time">00.00</p>         
+         </div>
+         <button class="box-game__button" id="submit-button" type="submit">Начать заново</button>
+     </header>
+     <div class="game-cards__suits center" id="suits">${cardsFlipSide}</div>
+    </div> `;
     appEl.innerHTML = appHTML;
+
+
+const reverseSlideCards = document.querySelectorAll('.game-cards__flip-side')
+
+
+
+for (const reverseSlideCard of reverseSlideCards) {
+    reverseSlideCard.addEventListener('click', () => {
+
+       let cardsSuitsArraySort = cardsSuitsArr.sort(() => Math.random() - 0.5);
+        document.getElementById('suits').innerHTML = `${cardsSuitsArraySort.slice(0, level.value)}`;
+
+        setTimeout(() => {
+            renderLevelGame(level, appEl);
+           }, 5000);
+    })
+
+}
+
+
+
+
+
+
 }
