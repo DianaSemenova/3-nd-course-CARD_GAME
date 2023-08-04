6237596749b117fcff48b3ad7ec3225ed12b41fc
Diana <menestrelia199702@gmail.com>
2023-08-04 12:30:07 +0400

    натсроена точка входа в вебпак

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
+const path = require("path");
+
 module.exports = {
     entry: "./index.js",
     module: {
         rules: [
-          { test: /\.scss$/, use: [ 'style-loader', 'scss-loader' ] },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: "asset/resource",
-          },
-        ]
-      },
+            { test: /\.scss$/, use: ["style-loader", "scss-loader"] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 
     output: {
+        path: path.resolve(__dirname, "dist"),
         filename: "main.js",
-    },
+        clean: true,
+    }
 };
