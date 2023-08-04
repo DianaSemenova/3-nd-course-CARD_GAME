d79136d00107ee10ad179f76eac1a5965634b572
Diana <menestrelia199702@gmail.com>
2023-08-04 12:25:55 +0400

    настроены лоадеры sass, изображения, шрифты

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 module.exports = {
     entry: "./index.js",
+    module: {
+        rules: [
+          { test: /\.scss$/, use: [ 'style-loader', 'scss-loader' ] },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+          },
+        ]
+      },
+
     output: {
         filename: "main.js",
     },
