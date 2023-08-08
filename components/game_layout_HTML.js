5b0c5cf366a7cb900d39e9f92e877515b38007ce
Diana <menestrelia199702@gmail.com>
2023-08-08 15:31:25 +0400

    функция с HTML выведена в отдельный модуль

---
+++ components/game_layout_HTML.js
@@ -0,0 +1,19 @@
+export function getLayoutHTML(duplicateCardsArrSort, appEl) {
+    const appHTML = `   
+    <div class="game-cards">
+     <header class="game-cards-timer">
+         <div class="game-cards-timer__module">  
+            <div class="game-cards-timer__init">
+                <p class="game-cards-timer__item">min</p>
+                <p class="game-cards-timer__item">sec</p>
+            </div>   
+             <p class="game-cards-timer__time">00.00</p>         
+         </div>
+         <button class="box-game__button" id="submit-button" type="submit">Начать заново</button>
+     </header>
+     <div class="game-cards__suits center" id="suits">${duplicateCardsArrSort.join(
+         "",
+     )}</div>
+    </div> `;
+    appEl.innerHTML = appHTML;
+}
--- components/render_level_game.js
+++ components/render_level_game.js
@@ -1,5 +1,6 @@
 import { shuffle } from "lodash";
 import { cardsSuitsArr } from "./array_cards_suits.js";
+import { getLayoutHTML } from "./game_layout_HTML.js";
 
 export function renderLevelGame(level, appEl, renderGameDifficulty) {
     let levelGame = level.value;
@@ -23,27 +24,32 @@ export function renderLevelGame(level, appEl, renderGameDifficulty) {
 
     getCardsFlipSideArr(levelGame);
 
-    const appHTML = `   
-    <div class="game-cards">
-     <header class="game-cards-timer">
-         <div class="game-cards-timer__module">  
-            <div class="game-cards-timer__init">
-                <p class="game-cards-timer__item">min</p>
-                <p class="game-cards-timer__item">sec</p>
-            </div>   
-             <p class="game-cards-timer__time">00.00</p>         
-         </div>
-         <button class="box-game__button" id="submit-button" type="submit">Начать заново</button>
-     </header>
-     <div class="game-cards__suits center" id="suits">${duplicateCardsArrSort.join(
-         "",
-     )}</div>
-    </div> `;
-    appEl.innerHTML = appHTML;
+    appEl.innerHTML = "";
+
+    // const appHTML = `
+    // <div class="game-cards">
+    //  <header class="game-cards-timer">
+    //      <div class="game-cards-timer__module">
+    //         <div class="game-cards-timer__init">
+    //             <p class="game-cards-timer__item">min</p>
+    //             <p class="game-cards-timer__item">sec</p>
+    //         </div>
+    //          <p class="game-cards-timer__time">00.00</p>
+    //      </div>
+    //      <button class="box-game__button" id="submit-button" type="submit">Начать заново</button>
+    //  </header>
+    //  <div class="game-cards__suits center" id="suits">${duplicateCardsArrSort.join(
+    //      "",
+    //  )}</div>
+    // </div> `;
+    // appEl.innerHTML = appHTML;
+
+    getLayoutHTML(duplicateCardsArrSort, appEl);
 
     let clickCards = true;
     let firstIndexCard = null;
     let secondIndexCard = null;
+    let counter = levelGame;
 
     const startOverGameButtons = document.getElementById("submit-button");
     startOverGameButtons.addEventListener("click", () => {
@@ -62,7 +68,6 @@ export function renderLevelGame(level, appEl, renderGameDifficulty) {
         for (const reverseSlideCard of reverseSlideCards) {
             reverseSlideCard.addEventListener("click", () => {
                 let cardsIndex = reverseSlideCard.dataset.index;
-
                 if (clickCards) {
                     console.log(clickCards);
                     cardsFlipSide[cardsIndex] =
@@ -80,7 +85,7 @@ export function renderLevelGame(level, appEl, renderGameDifficulty) {
                         duplicateCardsArrSort[cardsIndex];
 
                     secondIndexCard = cardsIndex;
-                    console.log(secondIndexCard);
+
                     document.getElementById(
                         "suits",
                     ).innerHTML = `${cardsFlipSide.join("")}`;
@@ -89,9 +94,15 @@ export function renderLevelGame(level, appEl, renderGameDifficulty) {
                         firstIndexCard,
                         secondIndexCard,
                         flipsСards,
+                        counter,
                     );
 
                     console.log(clickCards);
+
+                    counter = counter - 2;
+                    if (counter === 0) {
+                        alert("Выиграл");
+                    }
                 }
                 clickCards = !clickCards;
             });
@@ -100,13 +111,11 @@ export function renderLevelGame(level, appEl, renderGameDifficulty) {
 
     setTimeout(flipsСards, 5000);
 
-    function comparingTwoCard(firstIndexCard, secondIndexCard) {
+    function comparingTwoCard(firstIndexCard, secondIndexCard, counter) {
         if (cardsFlipSide[firstIndexCard] === cardsFlipSide[secondIndexCard]) {
             flipsСards();
-            alert("Выиграл");
         } else {
             alert("проиграл");
-            renderGameDifficulty(renderLevelGame);
         }
     }
 }
